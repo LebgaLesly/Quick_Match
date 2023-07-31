6d0a07cc260538572a6f8c7a090cec25364eb558
LebgaLesly <lebgalesly1@gmail.com>
2023-07-31 13:44:39 -0700

    Memory Game

---
+++ animals.js
@@ -0,0 +1,124 @@
+// Grabbing variables
+const section = document.querySelector('section');
+let playerLivesCount = document.querySelector('span');
+let playerLives = 9;
+
+// updating span
+playerLivesCount.textContent = playerLives;
+
+// Generating card data
+const getData = () => [
+    {imgSrc: "Images/Animals/1 - Copy.jpg", name:"lion" },
+    {imgSrc: "Images/Animals/11.png", name:"frog" },
+    {imgSrc: "Images/Animals/3 - Copy.jpeg", name: "zebra"},
+    {imgSrc: "Images/Animals/9.png", name: "snake"},
+    {imgSrc: "Images/Animals/5 - Copy.png", name: "monkey"},
+    {imgSrc: "Images/Animals/10.png", name:"crocodile" },
+    {imgSrc: "Images/Animals/7 - Copy.png", name: "cat"},
+    {imgSrc: "Images/Animals/8 - Copy.png", name: "dog"},
+    {imgSrc: "Images/Animals/1 - Copy.jpg", name:"lion" },
+    {imgSrc: "Images/Animals/11.png", name:"frog" },
+    {imgSrc: "Images/Animals/3 - Copy.jpeg", name: "zebra"},
+    {imgSrc: "Images/Animals/9.png", name: "snake"},
+    {imgSrc: "Images/Animals/5 - Copy.png", name: "monkey"},
+    {imgSrc: "Images/Animals/10.png", name:"crocodile" },
+    {imgSrc: "Images/Animals/7 - Copy.png", name: "cat"},
+    {imgSrc: "Images/Animals/8 - Copy.png", name: "dog"},
+];
+
+
+// Radomiziing the cards
+const randomize = () => {
+    const cardData = getData();
+    cardData.sort(() => Math.random() - 0.5)
+    return cardData
+}
+
+
+// Generating HTML for card
+const cardGenerator = () => {
+    const cardData = randomize();
+    cardData.forEach((item) => {
+        const card = document.createElement('div');
+        const face = document.createElement('img');
+        const back = document.createElement('div');
+        card.classList = "card";
+        face.classList = "face";
+        back.classList = "back";
+
+    //Attach infos to card
+    face.src = item.imgSrc
+    card.setAttribute('name' , item.name)
+    //Attach the cards to the section
+    section.appendChild(card);
+    card.appendChild(face);
+    card.appendChild(back);
+
+
+    // Adding the toggleClass
+    card.addEventListener('click', (e) => {
+        card.classList.toggle('toggleCard');
+        checkCards(e);
+    });
+
+});
+
+}; 
+
+// checking matching cards
+const checkCards = (e) => {
+    const clickedCard = e.target;
+    clickedCard.classList.add('flipped');
+    const flippedCards = document.querySelectorAll('.flipped')
+    const toggleCard = document.querySelectorAll('.togglecard')
+    //logic
+    if(flippedCards.length === 2){
+        if(flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')){
+            flippedCards.forEach(card => {
+                card.classList.remove('flipped')
+                card.style.pointerEvents = 'none'
+            })
+        } else{
+            flippedCards.forEach(card => {
+                card.classList.remove('flipped')
+                setTimeout(() => card.classList.remove('toggleCard'), 800)
+                
+            })
+            playerLives --;
+            playerLivesCount.textContent = playerLives
+            if(playerLives === 0){
+                restart("Sorry, You lost  😭")
+            }
+        }
+    }
+    // check if game is won
+    if(toggleCard.length === 16){
+        restart("Congratulations, you Won 😉")
+    }
+}
+
+
+// Restart
+const restart = (text) => {
+    let cardData = randomize();
+    let faces = document.querySelectorAll(".face");
+    let cards = document.querySelectorAll('.card');
+    section.style.pointerEvents = 'none'
+    cardData.forEach((item, index) => {
+        setTimeout(() => {
+            cards[index].classList.remove('toggleCard');
+        }, 1000)
+        setTimeout(() => {
+            cards[index].style.pointerEvents = 'all'
+            faces[index].src = item.imgSrc;
+            cards[index].setAttribute('name', item.name);
+            section.style.pointerEvents = 'all';
+        }, 1000)
+    });
+    playerLives = 9
+    playerLivesCount.textContent =  playerLives
+    setTimeout(() => window.alert(text), 50)
+}
+
+
+cardGenerator();
